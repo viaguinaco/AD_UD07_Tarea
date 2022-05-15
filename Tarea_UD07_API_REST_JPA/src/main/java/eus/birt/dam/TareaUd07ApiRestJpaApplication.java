fab316212731bac83c0d98686a63277cd09086ce
Victor <viaguinaco@birt.eus>
2022-05-15 21:49:08 +0200

    Commit inicial

---
+++ Tarea_UD07_API_REST_JPA/src/main/java/eus/birt/dam/TareaUd07ApiRestJpaApplication.java
@@ -0,0 +1,13 @@
+package eus.birt.dam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaUd07ApiRestJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaUd07ApiRestJpaApplication.class, args);
+	}
+
+}
---
+++ Tarea_UD07_API_REST_JPA/src/test/java/esu/birt/dam/TareaUd07ApiRestJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package esu.birt.dam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaUd07ApiRestJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
